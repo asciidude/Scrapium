e128726e9840f83454a9376e8662ce8416870abc
_thread90 <39356292+thread90@users.noreply.github.com>
2021-01-30 16:36:11 -0600

    init

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Scrapium
+# Scrapium - Web-scraping made easy
+Using and setting up Scrapium is as easy as 1, 2, 3!
+
+# Getting started
+```js
+const Scrapium = require('Scrapium');
+const { Keys } = require('Scrapium');
+
+const Start = async() => {
+    await Scrapium.browser('chrome');
+    await Scrapium.goto('https://www.google.com');
+    await Scrapium.get('q', true, ["Scrapium", Keys.RETURN]);
+}
+```
+**Very important:** ensure you have the webdriver of the browser you are trying to use, otherwise you'll get an error<br>
+I personally recommend [ChromeDriver](https://chromedriver.chromium.org/downloads) (uses Chromium)
+
+This can also be used for **automation**!
+
+# Docs
+`browser()` - Sets the web browser to use <br>
+`goto()` - Goes to a website <br>
+`get()` - Get an element on the website, send keys?, keys to send
+
+**For developers using this GitHub: Scrapium uses selenium-webdriver**
+
+**Note: I do not reccomend using this; you should use Puppeteer. This is still a prototype and has very little features**
---
+++ index.js
@@ -0,0 +1,37 @@
+const browsers = ["Chrome", "IE", "Edge", "Firefox", "Safari"];
+const {Builder, By, Key, util} = require('selenium-webdriver');
+let driver;
+
+const Keys = {};
+Object.assign(keys, Key);
+
+const browser = async(select="chrome", key="") => {
+    select = select.toLowerCase();
+    if(select == "firefox") {
+        driver = await new Builder().forBrowser('firefox').build();
+    } else if(select == "safari") {
+        driver = await new Builder().forBrowser('safari').build();
+    } else if(select == "ie" || select == "internetexplorer" || select == "internet explorer") {
+        driver = await new Builder().forBrowser('ie').build();
+    } else if(select == "chrome") {
+        driver = await new Builder().forBrowser('chrome').build();
+    } else {
+        return console.error("You must choose from this list for your browser: " + browsers.join(', '));
+    }
+}
+
+const goto = async(website="https://google.com") => {
+    await driver.get(website);
+}
+
+const get = async(element, sendKeys=false, send) => {
+    if(sendKeys) {
+        return await driver.findElement(By.name(element)).sendKeys(send.join('').toString());
+    } else {
+        return await driver.findElement(By.name(element));
+    }
+}
+
+module.exports = {
+    browser, goto, get, keys
+}
